c3b590e66965ef744321078b24acbb82e131363b
Kim Pepper <kim@previousnext.com.au>
2021-02-24 14:42:47 +1100

    Adds info cmd

---
+++ src/scripts/info.sh
@@ -0,0 +1,15 @@
+info() {
+    echo "Getting Skpr info for $SKPR_ENV"
+    # shellcheck disable=SC2086
+    SKPR_DOMAIN=$(skpr info $SKPR_ENV | jq -r ".Ingress.Domain")
+    SKPR_URL="https://$SKPR_DOMAIN"
+    echo "Skpr URL: $SKPR_URL"
+    echo "export SKPR_URL=$SKPR_URL" >> $BASH_ENV
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    info
+fi
