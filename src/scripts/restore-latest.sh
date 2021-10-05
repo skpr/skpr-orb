a583d400840ca80cf9407df6bc285b26aeaf9359
Karl Hepworth <karl.hepworth@gmail.com>
2021-10-05 15:29:48 +1100

    Add a command for restoring the latest backup
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

---
+++ src/scripts/restore-latest.sh
@@ -0,0 +1,22 @@
+restore() {
+    if [ -z "${SKPR_BACKUP}" ]; then
+        SKPR_BACKUP=$(skpr backup list "${SKPR_ENV}" --json | jq .[0].Name)
+        if [ $? -gt 0 ]; then
+          echo "Failed to get name of most recent backup"
+          exit 1
+        fi
+        # shellcheck disable=SC2086
+        echo "export SKPR_VERSION=$SKPR_VERSION" >> $BASH_ENV
+    fi
+    echo "Deploying backup: ${SKPR_VERSION}"
+
+    # shellcheck disable=SC2086
+    skpr deploy ${SKPR_ENV} ${SKPR_BACKUP}
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    restore
+fi
