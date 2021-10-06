e3f475e2c067482665e626ab9a6221ef01c6c64a
Karl Hepworth <karl.hepworth@gmail.com>
2021-10-06 13:20:07 +1100

    shfmt && shellcheck
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- src/scripts/restore.sh
+++ src/scripts/restore.sh
@@ -1,27 +1,29 @@
+#!/bin/env bash
+
 restore() {
-    if [ -z "${SKPR_BACKUP}" ]; then
-        SKPR_BACKUP=$(skpr backup list "${SKPR_SOURCE}" --json | jq  -r '[.][0][0].Name')
-        if [ $? -gt 0 ]; then
-          echo "Failed to get name of most recent backup"
-          exit 1
-        fi
-        # shellcheck disable=SC2086
-        echo "export SKPR_BACKUP=SKPR_BACKUP" >> $BASH_ENV
-    fi
-    echo "Restoring backup '${SKPR_BACKUP}' to ${SKPR_TARGET}"
+	if [ -z "${SKPR_BACKUP}" ]; then
+		SKPR_BACKUP=$(skpr backup list "${SKPR_SOURCE}" --json | jq -r '[.][0][0].Name')
+		if $? -gt 0; then
+			echo "Failed to get name of most recent backup"
+			exit 1
+		fi
+		# shellcheck disable=SC2086
+		echo "export SKPR_BACKUP=SKPR_BACKUP" >>$BASH_ENV
+	fi
+	echo "Restoring backup '${SKPR_BACKUP}' to ${SKPR_TARGET}"
 
-    if [ -z "${SKPR_SOURCE}" ] || [ -z "${SKPR_TARGET}" ]; then
-      echo "Failed: Source/Target environments are empty - cannot restore backup"
-      exit 1
-    fi
+	if [ -z "${SKPR_SOURCE}" ] || [ -z "${SKPR_TARGET}" ]; then
+		echo "Failed: Source/Target environments are empty - cannot restore backup"
+		exit 1
+	fi
 
-    # shellcheck disable=SC2086
-    skpr restore create ${SKPR_TARGET} ${SKPR_BACKUP}
+	# shellcheck disable=SC2086
+	skpr restore create ${SKPR_TARGET} ${SKPR_BACKUP}
 }
 
 # Will not run if sourced for bats-core tests.
 # View src/tests for more information.
 ORB_TEST_ENV="bats-core"
 if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
-    restore
+	restore
 fi
