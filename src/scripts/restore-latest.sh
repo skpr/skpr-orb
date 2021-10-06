151d9ffe5ff48a7618225b3c16b8bd255b4e603f
Karl Hepworth <karl.hepworth@gmail.com>
2021-10-06 12:45:15 +1100

    Update logic to take two environment names
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- src/scripts/restore-latest.sh
+++ src/scripts/restore-latest.sh
@@ -1,17 +1,17 @@
 restore() {
     if [ -z "${SKPR_BACKUP}" ]; then
-        SKPR_BACKUP=$(skpr backup list "${SKPR_ENV}" --json | jq .[0].Name)
+        SKPR_BACKUP=$(skpr backup list "${SKPR_SOURCE}" --json | jq .[0].Name)
         if [ $? -gt 0 ]; then
           echo "Failed to get name of most recent backup"
           exit 1
         fi
         # shellcheck disable=SC2086
-        echo "export SKPR_VERSION=$SKPR_VERSION" >> $BASH_ENV
+        echo "export SKPR_BACKUP=SKPR_BACKUP" >> $BASH_ENV
     fi
-    echo "Deploying backup: ${SKPR_VERSION}"
+    echo "Restoring backup '${SKPR_BACKUP}' to ${SKPR_TARGET}"
 
     # shellcheck disable=SC2086
-    skpr deploy ${SKPR_ENV} ${SKPR_BACKUP}
+    skpr restore create ${SKPR_TARGET} ${SKPR_BACKUP}
 }
 
 # Will not run if sourced for bats-core tests.
