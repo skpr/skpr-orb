6fd9223bfcb51e6c73f9754068da02b8a02c7cf0
Karl Hepworth <karl.hepworth@gmail.com>
2022-09-12 12:19:52 +1000

    Cleanup logic, remove default values & add a backup task.
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- src/scripts/restore.sh
+++ src/scripts/restore.sh
@@ -1,25 +1,13 @@
 #!/bin/env bash
 
 restore() {
-	if [ -z "${SKPR_BACKUP}" ]; then
-		SKPR_BACKUP=$(skpr backup list "${SKPR_SOURCE}" --json | jq -r '[.][0][0].Name')
-		if $? -gt 0; then
+	SKPR_BACKUP=$(skpr backup list "${SKPR_SOURCE}" --json | jq -r '[.][0][0].Name')
+	if $? -gt 0; then
 			echo "Failed to get name of most recent backup"
-			exit 1
-		fi
-		# shellcheck disable=SC2086
-		echo "export SKPR_BACKUP=${SKPR_BACKUP}" >>$BASH_ENV
-	fi
-
-	if [ -z "${SKPR_SOURCE}" ]; then
-		echo "Failed: Source environment is empty - cannot restore backup"
 		exit 1
 	fi
-
-	if [ -z "${SKPR_TARGET}" ]; then
-		echo "Failed: Target environment is empty - cannot restore backup"
-		exit 1
-	fi
+	# shellcheck disable=SC2086
+	echo "export SKPR_BACKUP=${SKPR_BACKUP}" >>$BASH_ENV
 
 	# shellcheck disable=SC2086
 	skpr restore create ${SKPR_TARGET} ${SKPR_BACKUP}
