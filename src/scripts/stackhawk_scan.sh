2579ef881e344181e78ee4975ed4311636d2d73b
Karl Hepworth <karl@previousnext.com.au>
2023-02-09 17:14:52 +1100

    pre-change change
    Signed-off-by: Karl Hepworth <karl@previousnext.com.au>

--- src/scripts/stackhawk_scan.sh
+++ src/scripts/stackhawk_scan.sh
@@ -3,7 +3,11 @@
 stackhawk_scan() {
 
   if [ ! "${PARAM_KEY}" == "" ]; then
-    docker run -e API_KEY="${PARAM_KEY}" --rm -v $(pwd):/hawk:rw -it stackhawk/hawkscan:latest
+
+    docker run --rm --detach -v /root/project/ --name data alpine:3.4 sleep 30;
+    docker cp /root/project/stackhawk.yml data:/stackhawk.yml;
+    docker run --rm -e API_KEY=$PARAM_KEY --volumes-from=data --name=stackhawk stackhawk/hawkscan:latest --repo-dir=/;
+
   else
     echo "API key was not provided."
   fi
