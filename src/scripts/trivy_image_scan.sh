f777fd32927abcf6c73219cc9c53aa89c979242c
Karl Hepworth <karl@previousnext.com.au>
2023-03-30 17:23:57 +1100

    Update to remove all version logic - so that the version must be provided by the pipeline
    Signed-off-by: Karl Hepworth <karl@previousnext.com.au>

--- src/scripts/trivy_image_scan.sh
+++ src/scripts/trivy_image_scan.sh
@@ -8,8 +8,8 @@ trivy_image_scan() {
     exit 1;
   fi
 
-  if [ "${PARAM_TYPE}" == "compile" ]; then
-    cat < skpr-manifest.json | jq -c ".[]" | jq "select(.type == \"compile\")" | jq  '.tag' -r > manifest-target.txt
+  if [ "${PARAM_TYPE}" == "compile" ] || [ "${PARAM_TYPE}" == "runtime" ]; then
+    cat < "skpr-manifest-${PARAM_TYPE}.json" | jq -c ".[]" | jq "select(.type == \"${PARAM_TYPE}\")" | jq  '.tag' -r > "manifest-target-${PARAM_TYPE}.txt"
     while IFS= read -r line
     do
       docker run --rm \
@@ -20,23 +20,7 @@ trivy_image_scan() {
               -e SKPR_PASSWORD="${SKPR_PASSWORD}" \
               "${SKPR_CLI_DOCKER_IMAGE}" \
               trivy image "${line}";
-    done < manifest-target.txt
-
-  fi
-
-  if [ "${PARAM_TYPE}" == "runtime" ]; then
-    cat < skpr-manifest.json | jq -c ".[]" | jq "select(.type == \"runtime\")" | jq  '.tag' -r > manifest-target.txt
-    while IFS= read -r line
-    do
-      docker run --rm \
-              -v /var/run/docker.sock:/var/run/docker.sock \
-              -v "$(pwd):$(pwd)" \
-              -w "$(pwd)" \
-              -e SKPR_USERNAME="${SKPR_USERNAME}" \
-              -e SKPR_PASSWORD="${SKPR_PASSWORD}" \
-              "${SKPR_CLI_DOCKER_IMAGE}" \
-              trivy image "${line}";
-    done < manifest-target.txt
+    done < "manifest-target-${PARAM_TYPE}.txt"
   fi
 
   if [ ! "${PARAM_TYPE}" == "compile" ] && [ ! "${PARAM_TYPE}" == "runtime" ]; then
